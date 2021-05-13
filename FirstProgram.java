fa041dddef55f2acad991a50bbbf0c0b9c8e06db
Thoriso-sudo <thorisochita00@gmail.com>
2021-05-14 00:32:48 +0200

    Programing is fun!

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+//First program on GitHub
+import javax.swing.*;
+public class FirstProgram
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Programing is fun!");
+		System.exit(0);
+	}
+}
